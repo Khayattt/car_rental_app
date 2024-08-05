d2e18b637ce3331d22473a3f5926f64ada45d625
Khayattt <Ahmadkhayat.ishtari@gmail.com>
2024-08-05 22:39:23 +0300

    Sync Pull

--- lib/views/CarDetails.dart
+++ lib/views/CarDetails.dart
@@ -1,6 +1,7 @@
 import 'package:car_rental_app/views/ColorSchema.dart';
 import 'package:flutter/material.dart';
 import 'ColorSchema.dart';
+import 'package:car_rental_app/modelViews/Cars_MV.dart';
 
 class CarDetails extends StatefulWidget {
   const CarDetails({super.key});
@@ -20,7 +21,27 @@ class _CarDetailsState extends State<CarDetails> {
         
       ),
       body: Center(
-        child: Text('This is a new page',style: TextStyle(color: Theme.of(context).colorScheme.tertiary),),
+        child: Column(
+          children: [
+            Center(
+              child: Text('This is a new page',style: TextStyle(color: Theme.of(context).colorScheme.tertiary),),
+            ),
+            Center(
+               child: ElevatedButton(
+            onPressed: () {
+              // Example car data
+              var carData = {
+                'make': 'Toyota',
+                'model': 'Corolla',
+                'year': 2020
+              };
+              addCar(carData); // Call the addCar function
+            },
+            child: Text('Add Car'),
+                ),
+            )
+          ],
+        ),
       ),
     );
   }
